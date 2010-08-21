0ddd41f6b2d28ed55f0e004096a032daaea4bc95
gtxx3600 <gtxx3600@gmail.com>
2010-08-21 21:35:36 +0800

    fix spell

---
+++ RM/Debug/RM_Manager.d
@@ -0,0 +1,10 @@
+RM_Manager.d RM_Manager.o: ../RM_Manager.c \
+ /home/hhf/workspace/SBPDB/include/rm.h \
+ /home/hhf/workspace/SBPDB/include/pf.h \
+ /home/hhf/workspace/SBPDB/include/sbpdb.h
+
+/home/hhf/workspace/SBPDB/include/rm.h:
+
+/home/hhf/workspace/SBPDB/include/pf.h:
+
+/home/hhf/workspace/SBPDB/include/sbpdb.h:
