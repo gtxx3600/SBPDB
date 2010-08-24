7e78ab32dcce810c94ee4a0538c6f6b82c3bf773
gtxx3600 <gtxx3600@gmail.com>
2010-08-24 19:02:26 +0800

    A brief architecture of IX

---
+++ IX/IX_IndexHandle.c
@@ -0,0 +1,25 @@
+/*
+ * IX_IndexHandle.c
+ *
+ *  Created on: 2010-8-24
+ *      Author: hhf
+ */
+#include "ix.h"
+RC IX_InsertEntry     (IX_IndexHandle* this, void *pData, const RID *rid){
+	return NORMAL;
+}
+RC IX_DeleteEntry     (IX_IndexHandle* this, void *pData, const RID *rid){
+	return NORMAL;
+}
+RC IX_ForcePages      (IX_IndexHandle* this){
+	return NORMAL;
+}
+
+
+RC initIX_IndexHandle(IX_IndexHandle * this)
+{
+	this->DeleteEntry = IX_DeleteEntry;
+	this->ForcePages = IX_ForcePages;
+	this->InsertEntry = IX_InsertEntry;
+	return NORMAL;
+}
--- include/ix.h
+++ include/ix.h
@@ -10,27 +10,46 @@
 
 #include "sbpdb.h"
 #include "pf.h"
+#include "rid.h"
 typedef struct IX_Manager IX_Manager;
 typedef struct IX_IndexHandle IX_IndexHandle;
-
+typedef struct IX_IndexScan IX_IndexScan;
 struct IX_Manager{
-	RC CreateIndex  (IX_Manager *this,
+	RC (*CreateIndex)(IX_Manager *this,
 						const char *fileName,
 						int        indexNo,
 						AttrType   attrType,
 						int        attrLength);
-	RC DestroyIndex (IX_Manager *this,
+	RC (*DestroyIndex)(IX_Manager *this,
 						const char *fileName,
 						int        indexNo);
-	RC OpenIndex    (IX_Manager *this,
+	RC (*OpenIndex)  (IX_Manager *this,
 						const char *fileName,
 						int        indexNo,
 						IX_IndexHandle *indexHandle);
-	RC CloseIndex   (IX_Manager *this,
+	RC (*CloseIndex) (IX_Manager *this,
 						IX_IndexHandle *indexHandle);
 };
 
 RC initIX_Manager(IX_Manager* this, PF_Manager* pfm);
 
+struct IX_IndexHandle{
+    RC (*InsertEntry)     (IX_IndexHandle* this, void *pData, const RID *rid);  // Insert new index entry
+    RC (*DeleteEntry)     (IX_IndexHandle* this, void *pData, const RID *rid);  // Delete index entry
+    RC (*ForcePages)      (IX_IndexHandle* this);                             // Copy index to disk
+};
 
+RC initIX_IndexHandle(IX_IndexHandle* this);
+
+struct IX_IndexScan{
+	RC (*OpenScan)		(IX_IndexScan* this,
+								const IX_IndexHandle *idxh,
+								CompOp op,
+								void* value,
+								ClientHint ch);
+	RC (*GetNextEntry)	(IX_IndexScan* this, RID *rid);
+	RC (*CloseScan)		(IX_IndexScan* this);
+};
+
+RC initIX_IndexScan(IX_IndexScan *this);
 #endif /* IX_H_ */
